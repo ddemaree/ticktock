c6dc0309d3a6c71e22f019300bfc98374eab250b
David Demaree <david@practical.cc>
2009-02-18 17:26:58 -0600

    More UI

--- config/routes.rb
+++ config/routes.rb
@@ -27,7 +27,7 @@ ActionController::Routing::Routes.draw do |map|
   map.resource :session, :account
   map.signup '/signup', :controller => 'accounts', :action => 'new'
 
-  # map.logout '/logout', :controller => 'sessions', :action => 'destroy'
+  map.logout '/logout', :controller => 'sessions', :action => 'destroy'
   # map.login '/login', :controller => 'sessions', :action => 'new'
   # map.register '/register', :controller => 'users', :action => 'create'
   # map.signup '/signup', :controller => 'users', :action => 'new'
