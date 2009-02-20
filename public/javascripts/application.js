86253859ad9ad0cd3c9cb73093735e109d91dadd
David Demaree <david@practical.cc>
2009-02-20 17:26:35 -0600

    Added disclosure stuff for event options

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -12,11 +12,6 @@ var TicktockGhostLabel = Class.create({
 		this.label.addClassName('ghost')
 		this.label.setStyle("position:absolute;top:0;left:0;z-index:22;")
 		
-		this.label.setStyle({
-			width:this.field.getWidth() + "px",
-			height:this.field.getHeight() + "px"
-		})
-		
 		this.label.observe('click', this.handleLabelClick.bind(this))
 		this.field.observe('blur', this.handleFieldBlur.bind(this))
 		this.field.observe('focus', this.handleFieldFocus.bind(this))
