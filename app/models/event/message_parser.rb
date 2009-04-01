e40b7bdafb58fe6d8e6f1d072c68090532394157
David Demaree <david@practical.cc>
2009-03-31 19:48:04 -0500

    Added newly ragelified message parser

--- app/models/event/message_parser.rb
+++ app/models/event/message_parser.rb
@@ -3,72 +3,20 @@ class Event::MessageParser
   
   # Parses message string into params
   def self.parse(params={})
+    if params.is_a?(String)
+      params = {:body => params}
+    end
+    
+    params[:source] = params[:body]
+    
+    params.merge!(Ticktock::Parser._parse(params[:body]))
+    params.symbolize_keys!
     params.reverse_merge!({
-      :body => "",
       :tags => Array(params[:tags]),
-      :user_name => nil,
-      :date => nil,
       :action => 'create'
     })
     
-    returning(params) do |output|
-      output[:source] = params[:body]
-      
-      return params if params[:body].nil?
-      
-      params[:body].sub!(/^(\w+)\b/) do |match|
-        case match
-          when 'start', 'stop', 'pause'
-            params[:action] = $1
-            ""
-          else
-            match
-        end
-      end
-      
-      # Unquoted tags without white space
-      params[:body].scan(/\#(?:(\w+))\s*/) do |match|
-        output[:tags] << $1; ""
-      end
-
-      # Quoted tags, which can contain spaces
-      params[:body].scan(/\#(?:\"(.*?)\"\s*)/) do |match|
-        output[:tags] << $1; ""
-      end
-      
-      # Trackables denoted with brackets
-      params[:body].sub!(/^(?:\[(.*?)\]\s*)/) do |match|
-        output[:subject] = $1; ""
-      end
-      
-      # Trackables denotes with Twitter-like @name syntax
-      params[:body].gsub!(/(?:@([a-zA-Z0-9_]+))\s*/) do |match|
-        output[:subject] = $1; ""
-      end
-      
-      # Duration
-      params[:body].sub!(TimeRegex) do |match|
-        output[:duration] = Event::TimeParser.from_string($1); ""
-      end
-      
-      # Date in MM/DD/YYYY
-      params[:body].gsub!(/(?:^|\W)(\d{1,2})(?:\/|-)(\d{1,2})(?:\/|-)(\d{2,4})/) do |match|
-        output[:date] = "#{$3}-#{$1.rjust(2,"0")}-#{$2.rjust(2,"0")}".to_date
-        ""
-      end
-      
-      # # Date using machine-readable format
-      # params[:body].sub!(/\b(?:d|date)\:(?:([\d-]+)\s*)/) do |match|
-      #   output[:date] = $1.to_date; ""
-      # end
-      # 
-      # # Date using natural language
-      # params[:body].sub!(/\b(?:d|date)\:(?:\"(.*?)\"\s*)/) do |match|
-      #   output[:date] = Chronic.parse($1).to_date; ""
-      # end
-
-      params[:body].strip!
-    end
+    HashWithIndifferentAccess.new(params)
   end
   
 end
--- test/unit/event/message_parser_test.rb
+++ test/unit/event/message_parser_test.rb
@@ -14,118 +14,88 @@ class Event::MessageParserTest < ActiveSupport::TestCase
     end
   end
   
-  context "Messages containing action" do
+  context "Ragel based message parser" do
+    
     setup do
-      @message = "start @GWOD Something #neat 0:15"
-      @params  = Event::MessageParser.parse(:body => @message)
+      @message = "@GWOD Something #neat 0:15"
+      @params  = Event::MessageParser.parse(@message)
     end
     
-    should "extract action" do
-      assert_equal "start", @params[:action]
+    should "have indifferent access" do
+      assert_not_nil @params[:body]
+      assert_not_nil @params["body"]
+      assert_equal @params[:body], @params["body"]
     end
     
-    should "preserve message" do
-      assert_equal "Something #neat", @params[:body]
-    end
-    
-    should "extract duration" do
-      assert_equal 15.minutes, @params[:duration]
-    end
   end
   
-  context "Messages containing tags" do
-    setup do
-      @message = "Hello world #taggable #shizzle #shizzle"
-      @params  = Event::MessageParser.parse(:body => @message)
-    end
-    
-    should "preserve message content with tags" do
-      assert_equal @message, @params[:body]
-    end
-    
-    should_preserve_message_source
-    
-    should "extract tags" do
-      assert_not_nil @params[:tags]
-      assert_equal 3, @params[:tags].length, @params[:tags].inspect
-      assert_contains @params[:tags], 'taggable'
-    end
-  end
-  
-  context "Messages containing trackable" do
-    context "using @ syntax" do
+    context "Messages containing tags" do
       setup do
-        @message = "Hello world @Practical"
+        @message = "Hello world #taggable #shizzle #shizzle"
         @params  = Event::MessageParser.parse(:body => @message)
       end
       
-      should_preserve_message
+      should "preserve message content with tags" do
+        assert_equal @message, @params[:body], @params.inspect
+      end
+      
       should_preserve_message_source
-
-      should "extract trackable" do
-        assert_equal "Practical", @params[:subject]
+       
+      should "extract tags" do
+        assert_not_nil @params[:tags]
+        assert_equal 3, @params[:tags].length, @params[:tags].inspect
+        assert_contains @params[:tags], 'taggable'
       end
     end
     
-    context "using [] syntax" do
-      setup do
-        @message = "[Practical] Hello world"
-        @params  = Event::MessageParser.parse(:body => @message)
-      end
-      
-      should_preserve_message
-
-      should "extract trackable" do
-        assert_equal "Practical", @params[:subject]
-      end
-    end
-  end
+    context "Messages containing trackable" do
+      context "using @ syntax" do
+        setup do
+          @message = "Hello world @Practical"
+          @params  = Event::MessageParser.parse(@message)
+        end
+        
+        should_preserve_message
+        should_preserve_message_source
   
-  context "Messages containing duration" do
-    context "in hh:mm:ss format" do
-      setup do
-        @message = "Working on a thing 2:00:00"
-        @params  = Event::MessageParser.parse(:body => @message)
-      end
-      
-      should "extract duration" do
-        assert_not_nil @params[:duration]
-      end
-      
-      should "return correct time in hours" do
-        assert_equal 2.hours, @params[:duration]
+        should "extract trackable" do
+          assert_equal "Practical", @params[:subject]
+        end
       end
     end
-    context "in hh:mm format" do
-      setup do
-        @message = "Working on a thing 2:00"
-        @params  = Event::MessageParser.parse(:body => @message)
+
+    context "Messages containing duration" do
+      context "in hh:mm:ss format" do
+        setup do
+          @message = "Working on a thing 2:00:00"
+          @params  = Event::MessageParser.parse(:body => @message)
+        end
+
+        should "extract duration" do
+          assert_not_nil @params[:duration]
+        end
+
+        should "return correct time in hours" do
+          assert_equal 2.hours, @params[:duration]
+        end
       end
       
-      should "extract duration" do
-        assert_not_nil @params[:duration]
-      end
-      
-      should "return correct time in hours" do
-        assert_equal 2.hours, @params[:duration]
+      context "in hh:mm format" do
+        setup do
+          @message = "Working on a thing 2:00"
+          @params  = Event::MessageParser.parse(:body => @message)
+        end
+
+        should "extract duration" do
+          assert_not_nil @params[:duration]
+        end
+
+        should "return correct time in hours" do
+          assert_equal 2.hours, @params[:duration]
+        end
       end
     end
-    
-    context "in 1.75h format" do
-      setup do
-        @message = "Working on a thing 1.75h"
-        @params  = Event::MessageParser.parse(:body => @message)
-      end
-      
-      should "extract duration" do
-        assert_not_nil @params[:duration]
-      end
-      
-      should "return correct time in hours" do
-        assert_equal 1.75.hours, @params[:duration]
-      end
-    end
-    
+
     context "Messages containing date" do
       context "in MM/DD/YYYY format" do
         setup do
@@ -159,37 +129,20 @@ class Event::MessageParserTest < ActiveSupport::TestCase
           end
         end
       end
-      
-      context "in MM-DD-YYYY format" do
-        setup do
-          @message = "12-3-1980 was born"
-          @params  = Event::MessageParser.parse(:body => @message)
-        end
-        
-        should "extract date" do
-          #flunk @params.inspect
-          assert_not_nil @params[:date]
-          assert_equal "1980-12-03".to_date, @params[:date]
-        end
-      end
+ 
+      # No longer supported
+      # context "in MM-DD-YYYY format" do
+      #   setup do
+      #     @message = "12-3-1980 was born"
+      #     @params  = Event::MessageParser.parse(:body => @message)
+      #   end
+      #   
+      #   should "extract date" do
+      #     #flunk @params.inspect
+      #     assert_not_nil @params[:date]
+      #     assert_equal "1980-12-03".to_date, @params[:date]
+      #   end
+      # end
     end
-
-    # TODO: Hook up this parser
-    # context "in 1h 25m format" do
-    #   setup do
-    #     @message = "Working on a thing 1h 25m"
-    #     @params  = Event::MessageParser.parse(:body => @message)
-    #   end
-    #   
-    #   should "extract duration" do
-    #     assert_not_nil @params[:duration]
-    #   end
-    #   
-    #   should "return correct time in hours" do
-    #     assert_equal((1.hours + 25.minutes), @params[:duration])
-    #   end
-    # end
-    
-  end
   
 end
